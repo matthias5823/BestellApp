005d7c9393531ceb5883fdbc9682dfeed72991e8
matthias5823 <matthias@dieorters.de>
2026-09-13 13:30:21 +0200

    Function created, which created the menu card

--- scr/JS/script.js
+++ scr/JS/script.js
@@ -10,10 +10,21 @@ function creatingButtonStructureMenu() {
 
 
 for (let indexFoodMenus = 0; indexFoodMenus < foodMenus.length; indexFoodMenus++) {
+    const menuCardRef = document.getElementById('food_menus');
     let menu = foodMenus[indexFoodMenus]['category'];
-    const navButtonRef = document.getElementById('structure_menu');
-    navButtonRef.innerHTML += `
-    <a href="">${menu}</a>
+    menuCardRef.innerHTML += `
+    <section id="menus_${menu}" class="" alt="Übersicht an Gerichten von ${menu}">
+        <header id="" class="menus-header">
+            <h3>${menu}</h3>
+        </header>
+    
+
+</section>
     `;
 
+}
+
+for (let indexMenu = 0; indexMenu < array.length; Menu++) {
+    
+    
 }
