ed811e46ce09b2e404c99a2b095d2a1765779c6a
matthias5823 <matthias@dieorters.de>
2026-09-18 18:44:03 +0200

    debug in Function removeDish

--- scr/JS/script.js
+++ scr/JS/script.js
@@ -3,6 +3,7 @@ function init() {
     creatingMenuCard();
 }
 
+// Erstellt den Container für die Kategorie der Gerichte
 function creatingStructureMenu() {
     for (let indexFoodMenus = 0; indexFoodMenus < foodMenus.length; indexFoodMenus++) {
         const menuCardRef = document.getElementById('food_menus');
@@ -18,6 +19,7 @@ function creatingMenuCard() {
     }
 }
 
+// Erstellt die Menukarten der jeweiligen Gerichte
 function setMenuCard(menusCard) {
     for (let indexMenu = 0; indexMenu < foodMenus[menusCard]['menus'].length; indexMenu++) {
         let foodName = foodMenus[menusCard]['menus'][indexMenu].menu;
@@ -25,14 +27,14 @@ function setMenuCard(menusCard) {
         let foodContent = foodMenus[menusCard]['menus'][indexMenu].foodContent;
         let price = foodMenus[menusCard]['menus'][indexMenu].price;
         price = price.toFixed(2);
-        let menuName = foodMenus[menusCard].category;
-        console.log(picSrc);
-        
+        let menuName = foodMenus[menusCard].category;        
         const menuCardRef = document.getElementById(`menus_${menuName}`);
         menuCardRef.innerHTML += renderMenuCard(foodName, foodContent, price, menusCard, indexMenu, picSrc);
     }
 }
 
+
+// Fügt das ausgewählte Gericht in die Variable Basket
 function addToBasket(menusCard, indexMenu) {
     const foodName = foodMenus[menusCard]['menus'][indexMenu].menu;
     const price = foodMenus[menusCard]['menus'][indexMenu].price.toFixed(2);
@@ -47,65 +49,55 @@ function addToBasket(menusCard, indexMenu) {
         basket[indexBusket].menge = basket[indexBusket].menge + 1;
         let menge = basket[indexBusket].menge;
         basket[indexBusket].totalPrice = basket[indexBusket].Price * menge;
-        console.log("Bereits vorhanden wird hinzugrfügt");
-        console.log(indexBusket);
-        console.log(foodName);
         changePriceContent(foodName, indexBusket);
 
     };
     calculateTotalDishesSum()
 }
 
+// Erstellt die Menukarte im Basket
+function setDishCard(buyFood, foodName, price) {
+    const renderCardRef = document.getElementById('order_basket_overview');
+    let totalPrice = buyFood * price;
+    renderCardRef.innerHTML += renderDishCard(buyFood, foodName, price, `${totalPrice.toFixed(2)} €`);
+}
+
+// Ändert den Preis wenn wein gericht bereits im Basket ist
 function changePriceContent(foodName, indexBusket) {
     const priceCardRef = document.getElementById(`total_${foodName}_price`);
     const mengeBasketHeaderCradRef = document.getElementById(foodName);
     const mengeBasketControllRef = document.getElementById(`${foodName}_controll`);
     let totalPrice = basket[indexBusket].price * basket[indexBusket].menge;
 
-    console.log(totalPrice.toFixed(2));
     mengeBasketControllRef.innerHTML = `${basket[indexBusket].menge}`;
     mengeBasketHeaderCradRef.innerHTML = `${basket[indexBusket].menge}`;
     priceCardRef.innerHTML = `${totalPrice.toFixed(2)} €`;
 }
 
-function setDishCard(buyFood, foodName, price) {
-    const renderCardRef = document.getElementById('order_basket_overview');
-    renderCardRef.innerHTML += renderDishCard(buyFood, foodName, price);
-}
 
-function removeToBasket(menusCard, indexMenu) {
-    const foodName = foodMenus[menusCard]['menus'][indexMenu].menu;
-    const price = foodMenus[menusCard]['menus'][indexMenu].price.toFixed(2);
-    let buyFood = foodMenus[menusCard]['menus'][indexMenu]['menge'];
+function removeDish(foodName) {
+let indexBusket = basket.findIndex(basket => basket.menu == foodName);
+if (indexBusket !== -1) {
+    basket.splice(indexBusket,1);
 
-    if (foodMenus[menusCard]['menus'][indexMenu]['menge'] <= 0) {
-        foodMenus[menusCard]['menus'][indexMenu]['menge'] = foodMenus[menusCard]['menus'][indexMenu]['menge'] - 1;
-        console.log("noch nicht bestellt wird hinzugefügt");
-        console.log(foodMenus[menusCard]['menus'][indexMenu]['menge'])
-
-    } else {
-        console.log("Bereits vorhanden wird hinzugrfügt");
-        foodMenus[menusCard]['menus'][indexMenu]['menge'] = foodMenus[menusCard]['menus'][indexMenu]['menge'] - 1;
-        console.log(foodMenus[menusCard]['menus'][indexMenu]['menge']);
-    };
-
-    console.log("im Warenkorb" + buyFood);
-
-
+};
+updateBasketCard();
+calculateTotalDishesSum();
 
 }
 
 
-function removeDish() {
+function updateBasketCard() {
 
     const renderCardRef = document.getElementById('order_basket_overview');
-    let indexBusket = basket.findIndex(basket => basket.menu == foodName);
+    renderCardRef.innerHTML = "";
 
     for (let indexBasket = 0; indexBasket < basket.length; indexBasket++) {
         let buyFood = basket[indexBasket].menge;
         let foodName = basket[indexBasket].menu;
         let price = basket[indexBasket].price;
-        renderCardRef.innerHTML += renderDishCard(buyFood, foodName, price);
+        let totalPrice = buyFood * price;
+        renderCardRef.innerHTML += renderDishCard(buyFood, foodName, price, totalPrice);
     }
 }
 
@@ -113,7 +105,6 @@ function calculateTotalDishesSum() {
     let totalSum = 0;
     for (let indexBasket = 0; indexBasket < basket.length; indexBasket++) {
         totalSum = totalSum + (basket[indexBasket].price * basket[indexBasket].menge);
-        console.log(totalSum);
     };
     addTotalDishesSum(totalSum);
     calculateTotalSum(totalSum);
@@ -129,17 +120,20 @@ let totalSum = totalDishSum
 
     if (totalSum <= 55.00) {
         totalSum = totalSum + 4.95;
-        addTotalSumDeliver(totalSum)
+        addTotalSumDeliver(totalSum);
     }else{
-
-
+        setDeliverPrice();
+        addTotalSumDeliver(totalSum);
     }
 }
 
 function addTotalSumDeliver(totalSum){
     const endFieldRef = document.getElementById('total_order');
     endFieldRef.innerHTML = `${totalSum.toFixed(2)} €`;
-
 }
 
+function setDeliverPrice() {
+    const deliverPriceRef = document.getElementById('discount_money');
+    deliverPriceRef.innerHTML = "0.00 €"    
+}
 
--- scr/JS/temp/dishesCard.js
+++ scr/JS/temp/dishesCard.js
@@ -1,4 +1,4 @@
-function renderDishCard(buyFood, foodName, price) {
+function renderDishCard(buyFood, foodName, price, totalPrice) {
     return `
         <section class="basket-card">
         <header class="basket-card-header">
@@ -7,13 +7,13 @@ function renderDishCard(buyFood, foodName, price) {
         <p id="${foodName}_price" class="none">${price} €</p>        
         <div class="basket-control-section">
             <div class="basket-control">
-                <button onclick="removeDish(menusCard, indexMenu, ${foodName})">delete</button>
-                <button onclick="removeToBasket(menusCard, indexMenu, ${foodName})">delete</button>
+                <button id="button_delete_dish" class="button-delete-dish" onclick="removeDish('${foodName}')">delete</button>
+                <button id="button_sub_dish" class"button-sub-dish none" onclick="removeToBasket('${foodName}')">sub</button>
                 <p id="${foodName}_controll">1</p>
-                <button onclick="addToBasket(menusCard, indexMenu, ${foodName})">Added</button>                
+                <button id="button_add_dish" class="button-add-dish" onclick="addToBasket('${foodName}')">Added</button>                
             </div>
             
-            <p id="total_${foodName}_price">19.50 €</p>
+            <p id="total_${foodName}_price">${totalPrice}</p>
         </div>
 
 
